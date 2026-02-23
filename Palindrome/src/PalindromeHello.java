e59170db01fac8021c71192881abcf5812180a25
rachita <rd1363@srmist.edu.in>
2026-02-23 09:34:16 +0530

    initial commit

---
+++ Palindrome/src/PalindromeHello.java
@@ -0,0 +1,7 @@
+public class PalindromeHello {
+    public static void main(String[] args) {
+        System.out.println("Welcome to palindrome");
+        system.out.println("version 1.0");
+        system.out.println("system initialize");
+    }
+}
