1199642c3b337ffb72209cebd2cce5a352ae97f5
rachita <rd1363@srmist.edu.in>
2026-02-23 09:48:08 +0530

    uc2 completed

--- Palindrome/src/PalindromeHello.java
+++ Palindrome/src/PalindromeHello.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class PalindromeHello {
     public static void main(String[] args) {
-        System.out.println("Welcome to palindrome");
-        system.out.println("version 1.0");
-        system.out.println("system initialize");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String original = sc.nextLine();
+
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equalsIgnoreCase(reversed)) {
+            System.out.println("It is a palindrome.");
+        } else {
+            System.out.println("It is not a palindrome.");
+        }
+
+        sc.close();
     }
 }
