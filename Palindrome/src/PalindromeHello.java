02a64b6adc582daa1e9e9671e7926d6b247cd847
seetharam9999 <undamatlakodandaseetharam1@gmail.com>
2026-02-23 10:56:10 +0530

    uc2-done

--- Palindrome/src/PalindromeHello.java
+++ Palindrome/src/PalindromeHello.java
@@ -1,35 +1,22 @@
 import java.util.Scanner;
-import java.util.Stack;
 
-public class PalindromeHello{
+public class PalindromeHello {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        Stack<Character> stack = new Stack<>();
 
-        System.out.print("Enter a string: ");
-        String input = sc.nextLine();
+        System.out.print("Enter a word: ");
+        String original = sc.nextLine();
 
-        // Push characters into stack
-        for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+        String reversed = "";
+
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed = reversed + original.charAt(i);
         }
 
-        boolean isPalindrome = true;
-
-        // Pop and compare
-        for (int i = 0; i < input.length(); i++) {
-            if (input.charAt(i) != stack.pop()) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        // Print result
-        if (isPalindrome) {
-            System.out.println("The string is a palindrome.");
+        if (original.equalsIgnoreCase(reversed)) {
+            System.out.println("It is a palindrome.");
         } else {
-            System.out.println("The string is not a palindrome.");
+            System.out.println("It is not a palindrome.");
         }
 
         sc.close();
