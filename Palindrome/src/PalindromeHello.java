df8bd03db696117e1623ccba89e0b492ba959911
seetharam9999 <undamatlakodandaseetharam1@gmail.com>
2026-03-10 10:13:40 +0530

    palindrome encapsulate

--- Palindrome/src/PalindromeHello.java
+++ Palindrome/src/PalindromeHello.java
@@ -2,37 +2,29 @@ import java.util.Scanner;
 
 public class PalindromeHello {
 
-    public static void main(String[] args) {
+    static boolean checkPalindrome(String text) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a string: ");
-        String str = sc.nextLine();
-
-        // Normalize string
-        str = str.toLowerCase();
-        str = str.replaceAll("\\s+", "");
-
-        int start = 0;
-        int end = str.length() - 1;
-        boolean palindrome = true;
+        int start = 0, end = text.length() - 1;
 
         while (start < end) {
-
-            if (str.charAt(start) != str.charAt(end)) {
-                palindrome = false;
-                break;
-            }
+            if (text.charAt(start) != text.charAt(end))
+                return false;
 
             start++;
             end--;
         }
 
-        if (palindrome)
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        if (checkPalindrome(str))
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
-
-        sc.close();
     }
 }
