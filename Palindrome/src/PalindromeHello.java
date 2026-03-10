cad75f134f9971f68ecdb86926872234cede3fdd
seetharam9999 <undamatlakodandaseetharam1@gmail.com>
2026-03-10 10:01:31 +0530

    palindrome ignore

--- Palindrome/src/PalindromeHello.java
+++ Palindrome/src/PalindromeHello.java
@@ -2,22 +2,6 @@ import java.util.Scanner;
 
 public class PalindromeHello {
 
-    static boolean isPalindrome(String str, int start, int end) {
-
-        // Base condition
-        if (start >= end) {
-            return true;
-        }
-
-        // Check characters
-        if (str.charAt(start) != str.charAt(end)) {
-            return false;
-        }
-
-        // Recursive call
-        return isPalindrome(str, start + 1, end - 1);
-    }
-
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -25,11 +9,29 @@ public class PalindromeHello {
         System.out.print("Enter a string: ");
         String str = sc.nextLine();
 
-        if (isPalindrome(str, 0, str.length() - 1)) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not Palindrome");
+        // Normalize string
+        str = str.toLowerCase();
+        str = str.replaceAll("\\s+", "");
+
+        int start = 0;
+        int end = str.length() - 1;
+        boolean palindrome = true;
+
+        while (start < end) {
+
+            if (str.charAt(start) != str.charAt(end)) {
+                palindrome = false;
+                break;
+            }
+
+            start++;
+            end--;
         }
+
+        if (palindrome)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
 
         sc.close();
     }
